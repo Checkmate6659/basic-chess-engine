e9d786586d4d669aa08a5b4b99c0e625a59b390a
EnigmaChess6659 <73489554+Checkmate6659@users.noreply.github.com>
2023-12-25 11:11:45 +0100

    Start

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "chess.hpp"
+
+using namespace chess;
+
+int main () {
+    Board board = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+    Movelist moves;
+    movegen::legalmoves(moves, board);
+
+    for (const auto &move : moves) {
+        std::cout << uci::moveToUci(move) << std::endl;
+    }
+
+    return 0;
+}
