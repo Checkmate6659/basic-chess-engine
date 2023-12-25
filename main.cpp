562795cd015d31f0225c98c9d75f5f369b5a86ed
EnigmaChess6659 <73489554+Checkmate6659@users.noreply.github.com>
2023-12-25 13:53:18 +0100

    Added UCI support except for GO command

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <ctime>
+#include <functional>
+#include <iostream>
 #include <time.h>
 
 #include "chess.hpp"
 #include "search.hpp"
 #include "eval.hpp"
 using namespace chess;
+
+#define ENGINE_NAME "BasicChessEngine v1.0"
 
 int main () {
     init_tables();
@@ -33,6 +37,58 @@ int main () {
     std::cout << score << std::endl;
     std::cout << nodes << std::endl;
     std::cout << nodes * CLOCKS_PER_SEC / (end - start) << std::endl;
+
+    //UCI loop
+    while(true)
+    {
+		std::string input_string;
+		std::getline(std::cin, input_string);
+		std::stringstream input_stream(input_string);
+		std::string command;
+		input_stream >> command;
+
+        switch (command[0])
+        {
+            case 'u': //uci (ucinewgame is here too; don't care)
+            std::cout << "id name " ENGINE_NAME "\nid author Enigma\n";
+            //TODO: if there are options, add them HERE!
+            std::cout << "uciok\n";
+            break;
+            case 'i': //isready
+            std::cout << "readyok\n";
+            break;
+            case 'q': //quit
+            return 0;
+            case 'p': //position
+            input_stream >> command; //get new word
+            if (command[0] == 's') //position startpos ...
+                board.setFen(constants::STARTPOS); //handy
+            else //position fen <FEN> ... (ONLY ACCEPTS FEN WITH COUNTERS! otherwise it freaks out)
+            {
+                std::string fen_string;
+                for (int i = 0; i < 6; i++) //fen string has 6 "words"
+                {
+                    input_stream >> command;
+                    fen_string.append(command);
+                    if(i != 5) fen_string.append(" "); //spaces between words
+                }
+                board.setFen(fen_string);
+            }
+            //load moves after fen string (or startpos)
+            input_stream >> command;
+            if (command[0] != 'm') break; //no moves to load
+            input_stream >> command;
+            while (input_stream)
+            {
+                board.makeMove(uci::uciToMove(board, command)); //make move
+                input_stream >> command; //load next move in
+            }
+            //std::cout << board.getFen() << std::endl; //DEBUG
+
+            // break;
+            // case 'g': //go command
+        }
+    }
 
     return 0;
 }
