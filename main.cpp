0319d82d2474ffaa93388e73687e67f25de3bfe4
EnigmaChess6659 <73489554+Checkmate6659@users.noreply.github.com>
2023-12-26 11:59:15 +0100

    Removed testing stuff; "fixed" go depth and "implemented" go infinite

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <ctime>
 #include <functional>
 #include <iostream>
@@ -15,28 +16,28 @@ int main () {
 
     //Board board = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     Board board = Board("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ");
-    std::cout << board.isRepetition(1); //count==1 => 2fold repetition
-    board.makeMove(uci::uciToMove(board, "f3e3"));
-    board.makeMove(uci::uciToMove(board, "e7f8"));
-    board.makeMove(uci::uciToMove(board, "e3f3"));
-    board.makeMove(uci::uciToMove(board, "f8e7"));
-    std::cout << board.isRepetition(1) << std::endl;
+    // std::cout << board.isRepetition(1); //count==1 => 2fold repetition
+    // board.makeMove(uci::uciToMove(board, "f3e3"));
+    // board.makeMove(uci::uciToMove(board, "e7f8"));
+    // board.makeMove(uci::uciToMove(board, "e3f3"));
+    // board.makeMove(uci::uciToMove(board, "f8e7"));
+    // std::cout << board.isRepetition(1) << std::endl;
 
-    std::cout << eval(board) << std::endl;
+    // std::cout << eval(board) << std::endl;
 
-    Movelist moves;
-    movegen::legalmoves(moves, board);
+    // Movelist moves;
+    // movegen::legalmoves(moves, board);
 
-    for (const auto &move : moves) {
-        std::cout << uci::moveToUci(move) << std::endl;
-    }
+    // for (const auto &move : moves) {
+    //     std::cout << uci::moveToUci(move) << std::endl;
+    // }
 
-    clock_t start = clock();
-    Value score = search(board, 4, -INT32_MAX, INT32_MAX);
-    clock_t end = clock();
-    std::cout << score << std::endl;
-    std::cout << nodes << std::endl;
-    std::cout << nodes * CLOCKS_PER_SEC / (end - start) << std::endl;
+    // clock_t start = clock();
+    // Value score = search(board, 4, -INT32_MAX, INT32_MAX);
+    // clock_t end = clock();
+    // std::cout << score << std::endl;
+    // std::cout << nodes << std::endl;
+    // std::cout << nodes * CLOCKS_PER_SEC / (end - start) << std::endl;
 
     //UCI loop
     while(true)
@@ -87,8 +88,8 @@ int main () {
             break;
             case 'g': //go command
             char engine_color = (board.sideToMove() == Color::WHITE) ? 'w' : 'b';
-            unsigned engtime = 1000; //engine's think time
-            unsigned enginc = 0;
+            unsigned engtime = 0; //engine's think time
+            unsigned enginc = INT32_MAX; //engine's increment (massive)
             unsigned movestogo = 30; //default to 30
             unsigned depth = MAX_DEPTH; //default depth = max
             unsigned movetime = 0; //no set movetime = 0 (special value)
